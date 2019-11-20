1cd64bbf37c04b67ef575681a8f9f43c1a3d4448
Garubieru <gabrieldeusdeth@gmail.com>
2019-11-20 13:43:28 -0300

    'sra_list_to_mitofree.py' now has a flag for tsv files and sorts alphabetically the output based on organism's name

--- misc_scripts/sra_list_to_mitofree.py
+++ misc_scripts/sra_list_to_mitofree.py
@@ -4,10 +4,18 @@ import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser(description="Converts SRA Runinfo Tables (downloadable from SRA Run Selector) into MitoFree tables. Note that seed accessions still need to be manually added to the output.")
-parser.add_argument("table", type=str, metavar="table", help="Path to Runinfo table (must be a comma-separated values table)")
+parser.add_argument("-t", "--tab", action="store_true", default=False, help="Converts tab-delimited tables (Default: comma-separated tables)")
+parser.add_argument("table", type=str, metavar="table", help="Path to Runinfo table")
 args = parser.parse_args()
 
-df = pd.read_csv(args.table)
-for run, organism in zip(df.Run, df.Organism):
+if args.tab:
+    df = pd.read_csv(args.table, sep='\t')
+else:
+    df = pd.read_csv(args.table)
+
+zipped = zip(df.Run, df.Organism)
+
+
+for run, organism in sorted(zipped, key = lambda x: x[1]):
     print("{}\t{}".format(run, organism.replace(" ", "_")))
                         
